6ba15b05e351deb0cf5427c49a63551d7324fddf
Emil Pirfält <emil.pirfalt@gmail.com>
2021-05-27 23:27:32 +0200

    Reorder functions in node server

--- app/index.js
+++ app/index.js
@@ -1,8 +1,5 @@
 const http = require("http");
 
-// Print env
-console.log(JSON.stringify(process.env, null, 2));
-
 // Create server
 const server = http.createServer((req, res) => {
   printReq(req);
@@ -13,6 +10,9 @@ const server = http.createServer((req, res) => {
 server.listen(process.env.PORT ?? 8000, () => {
   console.log(`Listening on: ${JSON.stringify(server.address())}`);
 });
+
+// Print env
+console.log(JSON.stringify(process.env, null, 2));
 
 // -----
 
